8f8145db3ebd99e43c035b8d47795acf301e0870
Mark Hildreth <mark.k.hildreth@gmail.com>
2013-04-27 00:42:07 -0400

    Springboards now spring. Player now falls. Infinite loops now more infinite.

--- scripts/game.js
+++ scripts/game.js
@@ -4,6 +4,8 @@ define(['graphics', 'assets'], function(gfx, assets) {
 	var G2C2D = function(d) {
 		return [d[0], 600 - d[1]]
 	};
+	var KEY_LEFT = 37,
+		KEY_RIGHT = 39;
 
 
 	return {
@@ -38,7 +40,14 @@ define(['graphics', 'assets'], function(gfx, assets) {
 						{type : 'air'},
 					],
 				],
-				player : [0, 1],
+				player : {
+					x : 0,
+					y : 1
+				},
+				actions : {
+					moveLeft : false,
+					moveRight : false,
+				},
 			};
 
 			return g;
@@ -73,27 +82,46 @@ define(['graphics', 'assets'], function(gfx, assets) {
 				}
 			}
 
-			var playerDest = G2C2D([g.player[0] * OFFSET + 25, g.player[1] * 50 + 25])
+			var playerDest = G2C2D([g.player.x * OFFSET + 25, g.player.y * 50 + 25])
 			gfx.drawSquare(ctx, playerDest, OFFSET - 20, "blue");
 		},
 
 		events : function(g) {
 			var lookup = {};
-			/*
 			var keydown = lookup.keydown = {};
-			keydown[KEY_UP] =function(g) { g.player.propulsing = true; }; 
-			keydown[KEY_LEFT] = function(g) { g.player.rotatingLeft = true; };
-			keydown[KEY_RIGHT] = function(g) { g.player.rotatingRight = true; };
-
-			var keyup = lookup.keyup = {};
-			keyup[KEY_UP] = function(g) { g.player.propulsing = false; };
-			keyup[KEY_LEFT] = function(g) { g.player.rotatingLeft = false; };
-			keyup[KEY_RIGHT] = function(g) { g.player.rotatingRight = false; };
-			*/
+			keydown[KEY_LEFT] = function(g) { g.actions.moveLeft = true; };
+			keydown[KEY_RIGHT] = function(g) { g.actions.moveRight = true; };
 			return lookup;
 		},
 
 		update : function(g, delta) {
+			if (g.actions.moveLeft) {
+				g.player.x = g.player.x - 1
+			}
+
+			if (g.actions.moveRight) { 
+				g.player.x = g.player.x + 1
+			}
+
+			var info = g.level[g.player.y - 1][g.player.x];
+			while (info.type !== 'ground') {
+				if (info.type === 'spring') {
+					info.type = 'ground';
+					g.player.y = g.player.y + 2;
+					if (g.actions.moveLeft) {
+						g.player.x = g.player.x - 1;
+					} else {
+						g.player.x = g.player.x + 1;
+					}
+				} else if (info.type === 'air') {
+					g.player.y = g.player.y - 1;
+				}
+
+				info = g.level[g.player.y - 1][g.player.x];
+			}
+
+			g.actions.moveLeft = false;
+			g.actions.moveRight = false;
 		}
 	};
 });
